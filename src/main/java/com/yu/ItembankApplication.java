db2fc2ca9b9451814ae35fa90b909c0b2177a515
yankai <yankai@ti-net.com.cn>
2019-05-10 14:55:44 +0800

    test

---
+++ src/main/java/com/yu/ItembankApplication.java
@@ -0,0 +1,13 @@
+package com.yu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItembankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItembankApplication.class, args);
+	}
+
+}
