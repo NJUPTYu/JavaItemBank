9b8708655c31809fbf8f97802f07410a780e2ac2
yankai <yankai@ti-net.com.cn>
2019-05-10 15:00:25 +0800

    test

---
+++ src/main/java/com/yu/test/Test.java
@@ -0,0 +1,11 @@
+package com.yu.test;
+
+/**
+ * @author YuGY
+ * @date 2019/05/10
+ */
+public class Test {
+    public static void main(String args[]){
+        System.out.println("Test");
+    }
+}
