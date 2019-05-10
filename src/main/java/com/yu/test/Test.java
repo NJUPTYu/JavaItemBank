81aeceabe7c5ddf6287abe3f11b7caeabdd0170a
yankai <yankai@ti-net.com.cn>
2019-05-10 15:01:27 +0800

    test

--- src/main/java/com/yu/test/Test.java
+++ src/main/java/com/yu/test/Test.java
@@ -7,5 +7,8 @@ package com.yu.test;
 public class Test {
     public static void main(String args[]){
         System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
     }
 }
